fed97e6df5991852e6a742c88d2111428bf4a49b
Joe Finny <finne132@umn.edu>
2018-06-16 21:11:20 -0500

    added fetch controller - going to call out to an external scraper script for returning results

---
+++ controllers/fetch.js
@@ -0,0 +1,36 @@
+"use strict"; 
+
+// fetch controller
+
+// require the database models
+var db = require("../models");
+
+// require the scraping script 
+var scrape = require("../scripts/scrape");
+
+// export the results of scraping 
+module.exports = {
+  scrapeHeadlines: function(req, res) {
+    return scrape()
+      .then(function(articles) {
+        return db.Headline.create(articles);
+      })
+      .then(function(dbHeadline) {
+        if (dbHeadline.length === 0) {
+          res.json({
+            message: "No results were returned"
+          });
+        }
+        else {
+          res.json({
+            message: "Scraping... Adding " + dbHeadline.length + " new articles to the database"
+          });
+        }
+      })
+      .catch(function(err) {
+        res.json({
+          message: "Scraping completed"
+        });
+      });
+  }
+};
