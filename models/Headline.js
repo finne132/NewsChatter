0c6e162ed1bbd65f3e01c792dfcd8e697b315fc6
Joe Finny <finne132@umn.edu>
2018-06-16 20:33:53 -0500

    created database schema

---
+++ models/Headline.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// require mongoose 
+var mongoose = require("mongoose");
+
+// declare mongoose database schema 
+var Schema = mongoose.Schema;
+
+// create a new mongoose database schema to store the Headlines 
+var headlineSchema = new Schema({
+  headline: {
+    type: String,
+    required: true,
+    unique: { index: { unique: true } }
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  photourl: {
+    type:String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  }
+});
+// set the schema to Headline variable
+var Headline = mongoose.model("Headline", headlineSchema);
+
+// export Headline variable as a module
+module.exports = Headline;
