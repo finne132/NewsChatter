b0632928819d36b96cc9a4a6010d62b9065a7144
Joe Finny <finne132@umn.edu>
2018-06-16 21:13:10 -0500

    added scripts/scraper.js - this will be where the actual scraping called from the fetch controller takes place

---
+++ scripts/scrape.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// scraper script that actually scrapes the NYTimes site based on the HTML tags I identified as being relevant 
+// This script gets called by the "fetch" controller to return results 
+
+// Write the script here ---------------
+
+// End Scraper Script ------------------
+
