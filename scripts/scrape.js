8ebd3d923b43e7f9ca4ca342ee5023723d74de06
Joe Finny <finne132@umn.edu>
2018-06-16 21:47:18 -0500

    finished scraper script

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -5,5 +5,55 @@
 
 // Write the script here ---------------
 
+// reference https://medium.com/@adinugroho/webscraping-with-nodejs-and-cheerio-c3971611736a
+
+// require axios 
+var axios = require("axios");
+// require cheerio 
+var cheerio = require("cheerio");
+
+var scrape = function() {
+    return axios.get("http://www.nytimes.com").then(function(res) {
+      var $ = cheerio.load(res.data);
+      var articles = [];
+      $(".theme-summary").each(function(i, element) {
+  
+        var head = $(this)
+          .children(".story-heading")
+          .text()
+          .trim();
+  
+        var url = $(this)
+          .children(".story-heading")
+          .children("a")
+          .attr("href");
+  
+        var sum = $(this)
+          .children(".summary")
+          .text()
+          .trim();
+  
+        if (head && sum && url) {
+  
+          var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+          var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+  
+          var dataToAdd = {
+            headline: headNeat,
+            summary: sumNeat,
+            url: url
+          };
+  
+          articles.push(dataToAdd);
+        }
+      });
+      return articles;
+    });
+  };
+  
+  
+  // export the module
+  module.exports = scrape;
+
 // End Scraper Script ------------------
 
